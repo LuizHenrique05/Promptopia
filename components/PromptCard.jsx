edc891ae5aa7b19e5390f7c6e52335c0c88d2698
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2023-08-15 16:37:25 -0300

    feat: create profile logic

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,6 +6,10 @@ import { useSession } from "next-auth/react"
 import { usePathname, useRouter } from "next/navigation"
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState('')
 
   const handleCopy = () => {
@@ -46,6 +50,12 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
       <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
       <p className='font-inter text-sm blue_gradient cursor-pointer' onClick={() => handleTagClick && handleTagClick(post.tag)}>{post.tag}</p>
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <button type="button" title="Edit Prompt" className='font-inter text-sm green_gradient cursor-pointer' onClick={handleEdit}>Edit</button>
+          <button type="button" title="Delete Prompt" className='font-inter text-sm orange_gradient cursor-pointer' onClick={handleDelete}>Delete</button>
+        </div>
+      )}
     </div>
   )
 }
